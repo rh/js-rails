9659e22269addd22409e5910ebefb8dcb3c970ca
Richard Hubers <richard.hubers@gmail.com>
2012-09-14 21:30:47 +0200

    Add initializer to fix routing errors

--- lib/js/rails/engine.rb
+++ lib/js/rails/engine.rb
@@ -1,6 +1,9 @@
 module Js
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'js-rails' do |app|
+        app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+      end
     end
   end
 end
